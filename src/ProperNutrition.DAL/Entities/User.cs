edcbe9a4a020e0af4329cab01f90ff074d5ecd7c
Hamidalion <fearofwar@mail.ru>
2020-10-06 21:26:15 +0300

    feat: add IdentityUser

---
+++ src/ProperNutrition.DAL/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProperNutrition.DAL.Entities
+{
+    /// <summary>
+    /// User by IdentityUser .
+    /// </summary>
+    public class User : IdentityUser
+    {
+    }
+}
