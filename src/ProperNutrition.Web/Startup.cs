25daa4072b8028fc558069f2f58beb67344faf88
Hamidalion <fearofwar@mail.ru>
2020-10-06 22:17:05 +0300

    feat: add ASP.NET Identity sys

--- src/ProperNutrition.Web/Startup.cs
+++ src/ProperNutrition.Web/Startup.cs
@@ -1,16 +1,34 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProperNutrition.DAL.Context;
+using ProperNutrition.DAL.Entities;
 
 namespace ProperNutrition.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration ?? throw new System.ArgumentNullException(nameof(configuration));
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ProperNutritionContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ProperNutritionConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ProperNutritionContext>();
+
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
