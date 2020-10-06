a834a26d21f1eff442bff0a6dfc5ed0521886993
Hamidalion <fearofwar@mail.ru>
2020-10-06 21:41:28 +0300

    feat: add DB context

---
+++ src/ProperNutrition.DAL/Context/ProperNutritionContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProperNutrition.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProperNutrition.DAL.Context
+{
+    /// <summary>
+    /// Proper Nutrition DBase context.
+    /// </summary>
+    public class ProperNutritionContext : IdentityDbContext<User>
+    {
+        /// <summary>
+        /// COnstructor whith param.
+        /// </summary>
+        /// <param name="options">Dbase context options.</param>
+        public ProperNutritionContext(DbContextOptions<ProperNutritionContext> options)
+       : base(options)
+        {
+        }
+    }
+}
